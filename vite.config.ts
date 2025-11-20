d3d322ab586a24540fc7e7fd159839f55d477913
rizaldyyakbar <muribar9@gmail.com>
2025-11-20 18:55:42 +0800

    feat: Add API proxy configuration to Vite for improved backend communication

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
         server: {
           proxy: {
+            "/api": "http://127.0.0.1:8000",
             "/weather": "http://127.0.0.1:8000",
             "/market": "http://127.0.0.1:8000",
     },
